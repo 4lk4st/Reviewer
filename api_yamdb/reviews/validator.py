86752df8f071cabfaa49ef58f98be03c9d374ac1
Dasha <dasha@MacBook-Pro.local>
2023-06-09 12:31:01 +0700

    add year_validator to TitleModel

--- api_yamdb/reviews/validator.py
+++ api_yamdb/reviews/validator.py
@@ -4,4 +4,4 @@ from django.utils import timezone
 
 def year_validator(value):
     if not (0 < value <= timezone.now().year):
-        raise ValidationError('Проверьте года создания произведения!')
+        raise ValidationError('Проверьте год создания произведения!')
